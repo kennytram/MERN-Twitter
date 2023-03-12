1d1b8085339de937ac58ad4d669ad24001ce8823
kennytram <kennyvtram@gmail.com>
2023-03-12 00:19:06 -0800

    seeding the database

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a user 13131232resource');
+  // res.send('respond with a user 13131232resource');
+  res.json({
+    message: "GET /api/tweets"
+  });
 });
 
 module.exports = router;
